063685b2214904d7025ef147c52bdf0dc28c607a
riyaagar <agar.riya62@gmail.com>
2020-02-24 00:38:36 -0500

    parstagram pt1

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Riya Agarwal on 2/23/20.
+//  Copyright © 2020 RAgarwal. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    
+    @IBOutlet weak var captionPostLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
